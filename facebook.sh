093c98f0ca6585170d71ff95726b4e1b6018e5c2
BrettPitt <57826915+BrettPitt@users.noreply.github.com>
2020-12-13 16:25:40 +0100

    Update facebook.sh

--- facebook.sh
+++ facebook.sh
@@ -1,8 +1,27 @@
-# was sind die Spalten?
+if dpkg -s csvkit 2>&1 ; then
+    echo "csvkit found"
+else
+    echo "csvkit not found"
+    exit 1
+fi
+declare -a menue=(
+    "Funktion: Datenvorschau"
+    "Funktion: Datenanalyse"
+)
+
+# was sind die spalten?
 csvcut -n facebook.csv
 
-# "inhalt des files anzeigen"
+#inhalt "anzeigen"
 csvlook facebook.csv | less -S
 
-#das enspricht aufgabe 7
-csvcut -c status_id,status_message,link_name,status_type,status_link,status_published,num_reactions,num_comments,num_shares,num_likes,num_loves,num_wows,num_hahas,num_sads,num_angrys facebook.csv | csvlook | head
+#das entspricht 7.1 -->  anzeigen der 3 sagen (status_type,status_link,status_published ) und pipen durch csvlook und head
+csvcut -c status_type,status_link,status_published facebook.csv | csvlook | head
+
+#7.2 teil 1 für anzahl status (inkl wie oft es vorkommt)-->
+csvcut -c status_type facebook.csv | csvstat
+#7.2 teil 2 anzahl wie viele status 
+csvcut -c status_id facebook.csv | csvstat
+
+#"vorbereitung für 7.3"
+csvcut -c num_reactions,num_comments,num_shares,num_likes,num_loves,num_wows,num_hahas,num_sads,num_angrys facebook.csv | csvstat --max
