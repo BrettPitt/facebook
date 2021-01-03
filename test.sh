335c089ea800fd06b0624b0c12d919955bab2d66
BrettPitt <57826915+BrettPitt@users.noreply.github.com>
2021-01-03 16:36:15 +0100

    7.1 7.2 7.3 are done

--- test.sh
+++ test.sh
@@ -1,19 +1,42 @@
-# if dpkg -s csvkit 2>&1 ; then
-#     echo "csvkit found"
-# else
-#     echo "csvkit not found"
-#     exit 1
-# fi
-TITLE="**Facebook Anal**"
-declare -a menu=(
+##Function Declarations
+
+# Task 7.1 get a Preview and each Column has a max Length of 10chars for better Overview
+function showPreview 
+    {
+        head -10 facebook.csv | csvlook --max-column-width 10
+    }
+
+# Task 7.2 get all types of Statuses and how often they appear 
+function showStatusTypes
+    {
+        csvsql --query "select count() as status_id, status_type from facebook group by status_type" facebook.csv | csvlook
+    }
+
+# Task 7.3 get the Post with the most attention
+function mostLiked 
+    {
+        #-F ignore commas in quotes
+        #then count all "_num" | Sort them descending | print the first
+        awk  -F "\"*,\"*" '{
+        print int($8 + $9 + $11 + $12 + $13 + $14 + $15), $1, $2
+        }' facebook.csv | sort -n -r | head -1
+
+                # awk ' /lebron/ {print $2}' facebook.csv
+    }
+
+
+# menu declaration
+TITLE="** Aufgabe 7 - Facebook Data Mining**"
+declare menu=(
     "7.1 Preview"
-    "7.2 Datenanalyse"
+    "7.1.1 Übersichtliches Preview" 
+    "7.2 Statustyp und Anzahl"
+    "7.3 Beliebtester Statuseintrag"
     "Beenden"
 )
 
 
 menuCount=${#menu[@]}
-
 while true; do
     echo "$TITLE"
     for((i=0; i<$menuCount; i++))
@@ -24,11 +47,41 @@ while true; do
     read ANTWORT
     case $ANTWORT in 
         0)
-            echo "blabla"
+        echo -e "\n=> ${MENU[0]}\n"
+            echo "Preview der ersten 10 Linien"
+            printf "\n"
+            head -n 10 facebook.csv | csvlook
+            printf "\n"
         ;;
         1)
-            echo "hihi"
+        echo -e "\n=> ${MENU[1]}\n"
+            echo "Preview mit beschränkter Zeilenlänge"
+            printf "\n"
+            showPreview 
+            printf "\n"
+
         ;;
+        2)
+        echo -e "\n=> ${MENU[2]}\n"
+            echo "Statustypen und Anzahl"
+            printf "\n"
+            showStatusTypes
+            printf "\n"
+        ;;
+        3)
+        echo -e "\n=> ${MENU[3]}\n"
+            echo "Meist bewerteter Kommentar"
+            printf "\n"
+            mostLiked
+            printf "\n"
+        ;;        
+        4)
+        echo -e "\n=> ${MENU[4]}\n"
+        break
     esac
     
 done
+
+
+
+
