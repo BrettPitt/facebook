1aef3ab02ef5a3038a0d49ec026e2464e78dbff2
BrettPitt <57826915+BrettPitt@users.noreply.github.com>
2021-01-07 16:47:10 +0100

    Add files via upload

---
+++ menu1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# menu.sh
+# Bash Menu
+#
+# 12.12.2018 / Lukas Müller
+#
+# Variablen
+# Beginn des Programmes
+# Menu ausgeben
+cat <<EOF
+* * * * * * * * * * * * * * * * * * * * * *
+* SkT Menu *
+* 1 Liste alle Shell Programme *
+* 2 neues Shell Skript erstellen *
+* e Ende *
+* * * * * * * * * * * * * * * * * * * * * *
+EOF
